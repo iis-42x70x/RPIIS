1e462f9e86009f8b680e77c6d7ce882203af6dc3
Andrew <andrewvvvw@gmail.com>
2024-11-12 22:24:22 +0300

    added script.sh

---
+++ sem1/lab5/script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Проверяем, что юзер передал оба параметра
+if [ -z "$1" ]; then
+    echo "Ошибка: Не указан путь к папке."
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "Ошибка: Не указан формат файла."
+    exit 1
+fi
+
+# Сохраняем параметры
+folder_path="$1"
+file_format="$2"
+
+# Проверяем, существует ли указанный путь к папке
+if [ ! -d "$folder_path" ]; then
+    echo "Ошибка: Указанная папка не существует."
+    exit 1
+fi
+
+# Поиск файлов с заданным форматом в указанной папке
+file_count=$(find "$folder_path" -type f -name "*.$file_format" | wc -l)
+echo "Количество файлов с форматом .$file_format в папке $folder_path: $file_count"
+
+# Проверяем условие
+if (($file_count % 2 == 0 )); then # число нужных файлов четное
+    number_of_processes1=$(ps -e --no-headers | wc -l) # число процессов
+    echo "Число активных процессов - $number_of_processes1"
+else # число нужных файлов нечетное
+    for (( i=7; i>=1; i-- ))
+    do
+        echo "Завершение программы через $i"
+        sleep 1
+    done
+    echo "Программа завершена."
+fi
+exit 0
