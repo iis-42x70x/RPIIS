0329e7670b0ac5b216f6a969e7ea4923864d9be1
dburbas <dima.burbas@gmail.com>
2024-11-21 13:41:03 +0300

    Lab5

---
+++ lab_5_/myscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ $# -ne 1 ]; then
+    echo "Неверное количество аргументов! Укажите размер файла в байтах."
+    exit 1
+fi
+file_size=$1
+dd if=text_template.txt of=generated_file.txt bs=1 count=$file_size
+minutes=$(stat -f "%SB" generated_file.txt|cut -d ":" -f 2)
+hours=$(stat -f "%SB" generated_file.txt|cut -d " " -f 3|cut -d ":" -f 1)
+if [ "$hours" -lt "$minutes" ]; then
+    mod_value_h_m=$((minutes - hours))
+else
+    mod_value_h_m=$((hours - minutes))
+fi
+number_of_directories=$((mod_value_h_m*file_size))
+for ((i=1;i<=number_of_directories;i++))
+do
+	path_dir="./$i"
+	mkdir -p "$path_dir"
+	cd "./$i"
+	touch "$hours"_"$minutes.txt"
+done
+echo "Гатово"
---
+++ lab_5_/myscriptB.bat
@@ -0,0 +1,25 @@
+@echo off
+set size=%1
+if "%size%"=="" (
+  echo Ne zadan parametr razmer of file!
+  exit /b
+)
+fsutil file createnew file.txt %size%
+for /f "tokens=1-5 delims=.: " %%j in ('
+ dir/a-d/tc "C:\Users\user\Desktop\file.txt"^| findstr/rc:"^[^ ]"
+') do (
+  set /a hours=%%m
+  set /a minutes=%%n
+)
+if "%hours%" lss "%minutes%" (
+  set /a mod_val=%minutes% - %hours%
+) else (
+set /a mod_val=%hours%-%minutes%
+)
+set /a num_dir=%mod_val%*%size%
+echo %num_dir%
+for /L %%i in (1,1,%num_dir%) do (
+  md %%i
+  cd %%i
+  echo. > "%hours%"-"%minutes%"
+)
