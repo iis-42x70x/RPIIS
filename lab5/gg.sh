cc752eb23f3a4143c99d4c1e13a5a43cc2528730
shukafgg <2007egor.sh@gmail.com>
2024-11-26 14:37:19 +0300

    gg.sh

---
+++ lab5/gg.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+    echo "Необходимы два аргумента: путь к первой и второй папке."    exit 1
+fi
+iz="$1"
+v="$2"
+
+if [ ! -d "$iz" ]; then    
+   echo "Данной папки нет - $iz"
+   exit 1 
+fi
+if [ ! -d "$v" ]; then
+    echo "Данной папки нет - $v"   
+    exit 1
+fi
+
+if [ -f "$file" ]; then        
+    if [[ "$file" == *.txt ]]; then
+           echo "Перемещение файла $(basename "$file") в $v"            
+           mv "$file" "$v"
+        else            
+          echo "Устанавливаем атрибут 'только чтение' для файла $(basename "$file")"
+          chmod 444 "$file"        
+fi
+fi 
+done
+echo "Выполнение завершено."
