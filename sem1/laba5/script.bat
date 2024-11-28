8b7f70d2ad3f0cfce8ea6e1255827ccee68441d3
suyeatolog <kirillfeofilov537@gmail.com>
2024-11-28 11:15:20 +0300

    fixedbat

--- sem1/laba5/script.bat
+++ sem1/laba5/script.bat
@@ -1,16 +1,15 @@
 @echo off
 if "%~1"=="" (
-    echo The path is missed. Try "%~0 path"
+    echo The path to storage is missed. Try "%~0 path"
     exit /b 1
 )
-
+set "folder=%~1"
 if not exist "%~1" (
     echo Directory is missed.
     exit /b 1
 )
 
 setlocal enabledelayedexpansion
-set "folder=%~1"
 set "count=0"
 
 for /r "%folder%" %%f in (*) do (
@@ -26,10 +25,11 @@ for /f "tokens=1-3 delims=." %%a in ("%date%") do (
 set max_digit=0
 for %%d in (%day% %month% %year%) do (
     for /l %%i in (0,1,9) do (
-        set "char=%%d"
-        set "char=!char:%%i=!"
-        if not "!char!"=="%%d" (
-            set /a max_digit=%%i
+        echo %%d | find "%%i" >nul
+        if not errorlevel 1 (
+            if %%i gtr !max_digit! (
+                set max_digit=%%i
+            )
         )
     )
 )
@@ -37,7 +37,7 @@ echo max date of today : "%max_digit%"
 set /a result=%count% * %max_digit%
 echo result = "%result%"
 set "output_file=%folder%\%count%.txt"
-echo "%result%" >> "%output_file%"
+echo %result% > "%output_file%"
 echo result is saved at "%output_file%"
 endlocal
 exit /b 0
