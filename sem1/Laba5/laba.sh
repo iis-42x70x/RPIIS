961c0889ddef0c8e28e110b31c0183f2c5e6eee4
Minna <mariagorbunova2007@gmail.com>
2024-12-02 20:48:32 +0300

    Update laba.sh

--- sem1/Laba5/laba.sh
+++ sem1/Laba5/laba.sh
@@ -4,7 +4,7 @@
 today=$(date +%d.%m.%Y)
 # Считаем активные процессы в ОС
 total=$(ps -e --no-header | wc -l)
-result="${today} ${total}"
+result="${today}_${total}"
 # Записываем результат в файл result.txt
 echo "$result" > result.txt
 echo "Результат записан в файл result.txt: $result"
