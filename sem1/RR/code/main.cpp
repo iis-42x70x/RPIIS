6de89f82a37561cdce5a52654e242057f3dc777a
Artemdjdj <artembasuraa@gmail.com>
2024-12-02 21:48:23 +0300

    work with files in RR

--- sem1/RR/code/main.cpp
+++ sem1/RR/code/main.cpp
@@ -1,3 +1,155 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <fstream>
+
+using namespace std;
+
+const int INF = 1e9;
+
+void ruchnoi(int &vertex, int &edge, vector<vector<pair<int, int>>> &graph, int &start) {
+    cout << "Введите количество вершин: ";
+    cin >> vertex;
+    cout << "Введите количество ребер: ";
+    cin >> edge;
+    graph.resize(vertex);
+
+    cout << "Введите ребра в формате: вершина1 вершина2 вес" << endl;
+    for (int i = 0; i < edge; i++) {
+        int a, b, weight;
+        cin >> a >> b >> weight;
+        graph[a].push_back({b, weight});
+        graph[b].push_back({a, weight});
+    }
+
+    cout << "Введите вершину, для которой вы хотите найти дерево кратчайших путей: ";
+    cin >> start;
+}
+void osnov_alg(int vertex, int edge, vector<vector<pair<int, int>>> &graph, int start) {
+    vector<int> dist(vertex, INF);
+    vector<int> pr(vertex, -1);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+
+    dist[start] = 0;
+    q.push({0, start});
+
+    while (!q.empty()) {
+        pair<int, int> g = q.top();
+        q.pop();
+
+        int d = g.first;
+        int v = g.second;
+
+        if (d > dist[v]) continue;
+
+        for (auto edg : graph[v]) {
+            int to = edg.first;
+            int weight = edg.second;
+            if (dist[to] > dist[v] + weight) {
+                dist[to] = dist[v] + weight;
+                pr[to] = v;
+                q.push({dist[to], to});
+            }
+        }
+    }
+
+    // Вывод кратчайших путей и расстояний
+    for (int finish = 0; finish < vertex; finish++) {
+        vector<int> path;
+        int cur = finish;
+
+        if (dist[finish] == INF) {
+            cout << "Вершина " << finish << " не достижима" << endl;
+            continue;
+        }
+
+        while (cur != -1) {
+            path.push_back(cur);
+            cur = pr[cur];
+        }
+
+        cout << "Кратчайший путь до вершины " << finish << ": ";
+        for (int i = path.size() - 1; i >= 0; i--) {
+            cout << path[i] << (i > 0 ? " -> " : "");
+        }
+        cout << " расстояние: " << dist[finish] <<  endl;
+    }
+    cout << endl;
+
+    cout << "Расстояния до вершин: ";
+    for (auto di: dist){//вывод  самих расстояний до вершин
+        if (di==INF){
+            cout << "X"<<" ";
+        }
+        else{
+            cout << di << " ";
+        }
+    }
+    cout << endl;
+}
+
+
+void read_file(int &vertex, int &edge, vector<vector<pair<int, int>>> &graph, int &start) {
+    ifstream file("input.txt");
+    if (!file.is_open()) {
+        cout << "Ошибка открытия файла" << endl;
+        return;
+    }
+
+    for (int i= 0;i < 5; i++) {
+        if (i> 0) { // пропускаем пустую строку между тестами
+            string emptyLine;
+            getline(file, emptyLine);
+        }
+
+        file >> vertex >> edge;
+        graph.clear();
+        graph.resize(vertex);
+
+        for (int j = 0; j < edge; j++) {
+            int a, b, weight;
+            file >> a >> b >> weight;
+            graph[a].push_back({b, weight});
+            graph[b].push_back({a, weight});
+        }
+        file >> start;
+        file.ignore(); // игнорируем символ новой строки после стартовой вершины
+        osnov_alg(vertex, edge, graph, start);
+    }
+
+    file.close();
+}
+
+
+int main() {
+    int cass, vertex = 0, edge = 0, start = 0;
+    vector<vector<pair<int, int>>> graph;
+
+    cout << "Введите 1, если хотите считать тестовые значения из файла" << endl;
+    cout << "Введите 2, если хотите вручную заполнять граф" << endl;
+    cin >> cass;
+
+    switch (cass) {
+        case 1: {
+            read_file(vertex, edge, graph, start);
+        }
+            break;
+        case 2: {
+            ruchnoi(vertex, edge, graph, start);
+            osnov_alg(vertex, edge, graph, start);
+        }
+            break;
+        default: {
+            ruchnoi(vertex, edge, graph, start);
+            osnov_alg(vertex, edge, graph, start);
+        }
+            break;
+    }
+
+    return 0;
+}
+
+
 /*
 #include <iostream>
 #include <vector>
@@ -172,100 +324,3 @@ while(from[cur] != -1) {
 
     return 0;
 }*/
-
-#include <iostream>
-#include <vector>
-#include <queue>
-
-using namespace std;
-
-const int INF = 1e9;
-
-int main(){
-            int vertex, edge;
-            cout << "Введите количество вершин: ";
-            cin >> vertex;
-            cout << "Введите количество ребер: ";
-            cin >> edge;
-            vector <vector<pair<int, int>>> graph(vertex);
-            for (int i = 0; i < edge; i++){
-                int a,b, weight;
-                cin >> a >> b>>weight;
-                graph[a].push_back({b, weight});
-                graph[b].push_back({a,weight});
-            }
-            vector <int> dist(graph.size(), INF);//создание массива,
-            // в котором будут хранится расстояния до вершин(изначально расстояния до всех вершин равно бесконечности)
-
-            priority_queue <pair<int, int>, vector <pair<int, int>>, greater<pair<int, int>> > q;//создание очереди с приоритетом
-
-            vector <int> pr(graph.size(), -1);//создание массива для восстановления кратчайших путей
-
-            int start;//инициализация вершины, для которой хотим найти расстояния до других вершин
-            cout <<"Введите вершину, для которой вы хотите найте дерево кратчайших путей: ";
-            cin>> start;
-            dist[start] = 0;//расстояние до стартовой вершины равно 0
-            q.push({0,start});//ставим в очередь нашу стартовую вершину
-
-            while(!q.empty()){//выполняем действия в цикле,  до тех пор пока очередь не пуста.
-                pair <int, int> g = q.top();// извлекаем информацию о вершине
-                q.pop();//удаляем вершину из очереди
-
-                int d = g.first; //расстояние до вершины
-                int v = g.second;//номер вершины
-
-                if (d> dist[v]) continue;//мы уже обработали эту вершину, используя другой путь
-                //перейти к следующей вершине
-
-                for (auto edg : graph[v]){//проход по всем соседним вершинам
-                    int to = edg.first;
-                    int weight = edg.second;
-                    if (dist[to]> dist[v] + weight){//проверяем: можно ли улучшить наше расстояние до соседа
-                        dist[to] = dist[v] + weight;
-                        pr[to]= v;// записываем вершину,из которой пришли
-                        q.push({dist[to], to});//помещаем новую вершину в очередь
-                    }
-                }
-
-            }
-
-
-
-
-            vector <int> path;//создание массива, в котором и будут храниться наши вершины, которые
-            //нужны для восстановления путей
-            for (int finish = 0; finish < vertex; finish++) {// для всех вершин
-                path.clear();//очистка массива path, для нахождения дерева кратчайших путей до следующей вершины
-                int cur = finish;
-                if (pr[finish] != -1 || finish==start){//проверка на не досягаемые вершины
-                    path.push_back(cur);
-                }
-                else{
-                    cout<<"Не досягаема";
-                }
-                //добавляем вершины до тез пор пока не встретим -1 в массиве pr(то есть пока не дойдем до конечной вершины)
-                while (pr[cur] != -1) {
-                    cur = pr[cur];
-                    path.push_back(cur);
-                }
-                vector <int> new_path;// создание нового массива, в котором будут храниться элементы массива
-                // path записанные в обратном порядке
-                for (int i= path.size()-1; i >=0; i--){
-                    new_path.push_back((path[i]));
-                }
-                for (auto f: new_path) {//вывод кратчайших путей(вершин, по которым проходят пути)
-                    cout << f << " ";
-                }
-                cout << endl;
-            }
-            cout << endl;
-            for (auto di: dist){//вывод  самих расстояний до вершин
-                if (di==INF){
-                    cout << "X"<<" ";
-                }
-                else{
-                    cout << di << " ";
-                }
-            }
-     return 0;
-}
