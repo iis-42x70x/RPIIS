4e9e28b3a506762e9bf792e0b3d137e1c6c62bc4
Minna <mariagorbunova2007@gmail.com>
2024-12-02 20:46:27 +0300

    Create laba.sh

---
+++ sem1/Laba5/laba.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Получаем текущую дату в формате
+today=$(date +%d.%m.%Y)
+# Считаем активные процессы в ОС
+total=$(ps -e --no-header | wc -l)
+result="${today} ${total}"
+# Записываем результат в файл result.txt
+echo "$result" > result.txt
+echo "Результат записан в файл result.txt: $result"
