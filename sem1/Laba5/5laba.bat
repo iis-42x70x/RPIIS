9f1b3558bb6ee74f584997c6d8432990a713d81a
Minna <mariagorbunova2007@gmail.com>
2024-12-03 20:42:03 +0300

    Update 5laba.bat

--- sem1/Laba5/5laba.bat
+++ sem1/Laba5/5laba.bat
@@ -1,11 +1,11 @@
 @echo off
-REM Получаем сегодняшнюю дату
-FOR /F "tokens=1,2 delims= " %%A IN ('date /t') DO SET today=%%B%%A
-REM Считаем количество активных процессов и сохраняем в переменную total.
-REM Используем команду 'tasklist' для получения списка процессов.
-REM 'find /c' считает количество строк.
-FOR /F %%i IN ('tasklist ^| find /v "Image Name" ^| find /v "===" ^| find /c /v ""') DO SET total=%%i
+rem Получаем сегодняшнюю дату
+for /F "tokens=1,2 delims= " %%A in ('date /t'do set today=%%B%%A
+rem Считаем количество активных процессов и сохраняем в переменную total.
+rem Используем команду 'tasklist' для получения списка процессов.
+rem 'find /c' считает количество строк.
+for /F %%i in ('tasklist ^| find /v "Image Name" ^| find /v "===" ^| find /c /v ""') do set total=%%i
 SET result=%today%_%total%
-REM Создаем файл result.txt и записываем туда результат.
+rem Создаем файл result.txt и записываем туда результат.
 echo %result% > result.txt
 echo Результат записан в файл result.txt: %result%
