7c37bc5c038d39ee688721d20c05f78a3f25d1bc
Minna <mariagorbunova2007@gmail.com>
2024-12-19 20:32:41 +0300

    Update Raschetka.cpp

--- sem1/RR/Raschetka.cpp
+++ sem1/RR/Raschetka.cpp
@@ -4,15 +4,15 @@
 
 using namespace std;
 
-int max_vertex_degree(const vector<vector<int>>& graph) {
-    int max_degree = 0; // Начальное значение для максимальной степени вершины
+int max_degree(const vector<vector<int>>& graph) {
+    int max_degree = 0; 
     for (const auto& neighbors : graph) {
-        max_degree = max(max_degree, static_cast<int>(neighbors.size())); // Сравниваем и обновляем максимальную степень
+        max_degree = max(max_degree, static_cast<int>(neighbors.size())); 
     }
-    return max_degree; // Возвращаем максимальную степень
+    return max_degree; 
 }
 
-void print_graph_and_max_degree(const vector<vector<int>>& graph) {
+void print(const vector<vector<int>>& graph) {
     cout << "Граф:" << endl;
     for (int i = 0; i < graph.size(); i++) {
         cout << "Вершина " << i << ": ";
@@ -21,7 +21,7 @@ void print_graph_and_max_degree(const vector<vector<int>>& graph) {
         }
         cout << endl;
     }
-    cout << "Максимальная степень вершины: " << max_vertex_degree(graph) << endl; // Печатаем максимальную степень
+    cout << "Максимальная степень вершины: " << max_degree(graph) << endl; 
 }
 
 void graph1(vector < vector<int> >& graph, int& v) {
@@ -34,7 +34,7 @@ void graph1(vector < vector<int> >& graph, int& v) {
     graph[4] = { 1,5 };
     graph[5] = { 1,4,6 };
     graph[6] = { 2,5 };
-    print_graph_and_max_degree(graph);
+    print(graph);
 }
 
 void graph2(vector < vector<int> >& graph, int& v) {
@@ -48,7 +48,7 @@ void graph2(vector < vector<int> >& graph, int& v) {
     graph[5] = { 4,6 };
     graph[6] = { 0,5,7 };
     graph[7] = { 6 };
-    print_graph_and_max_degree(graph);
+    print(graph);
 }
 
 void graph3(vector < vector<int> >& graph, int& v) {
@@ -59,7 +59,7 @@ void graph3(vector < vector<int> >& graph, int& v) {
     graph[2] = { 0 };
     graph[3] = { 0,1 };
     graph[4] = {};
-    print_graph_and_max_degree(graph);
+    print(graph);
 }
 
 void graph4(vector < vector<int> >& graph, int& v) {
@@ -71,7 +71,7 @@ void graph4(vector < vector<int> >& graph, int& v) {
     graph[3] = { 1,5 };
     graph[4] = { 0,5 };
     graph[5] = { 1,3,4 };
-    print_graph_and_max_degree(graph);
+    print(graph);
 }
 
 void graph5(vector < vector<int> >& graph, int& v) {
@@ -82,7 +82,7 @@ void graph5(vector < vector<int> >& graph, int& v) {
     graph[2] = { 1,3 };
     graph[3] = { 0,4 };
     graph[4] = { 3 };
-    print_graph_and_max_degree(graph);
+    print(graph);
 }
 
 
