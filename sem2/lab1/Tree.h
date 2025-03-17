e4ce6f61c20a8cec5aa4a8f1bd5d51b977460ce1
shkibidilnick <feechkawinks1987@gmail.com>
2025-03-17 22:29:33 +0300

    Create Tree.h

---
+++ sem2/lab1/Tree.h
@@ -0,0 +1,25 @@
+#ifndef TREE_HPP
+#define TREE_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+struct Node {
+    int l, r;       
+    int max;        
+    int add;       
+    Node* left;    
+    Node* right;   
+};
+
+
+Node* createNode(int l, int r);
+Node* buildTree(int l, int r, int* initialValues);
+void modify(Node* node, int l, int r, int val);
+int query(Node* node, int l, int r, int sumAdd);
+void collectValues(Node* root, int start, int end, int* output);
+
+#endif
