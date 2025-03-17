9ba54a7a853b3f4831954f6fda82e691565cb660
shkibidilnick <feechkawinks1987@gmail.com>
2025-03-17 22:35:42 +0300

    Create Tree.cpp

---
+++ sem2/lab1/Tree.cpp
@@ -0,0 +1,60 @@
+#include "Tree.h"
+
+Node* createNode(int l, int r) {
+    Node* node = new Node();
+    node->l = l;
+    node->r = r;
+    node->max = 0;
+    node->add = 0;
+    node->left = nullptr;
+    node->right = nullptr;
+    return node;
+}
+
+Node* buildTree(int l, int r, int* initialValues) {
+    Node* node = createNode(l, r);
+    if (l == r) {
+        node->max = initialValues[l];
+        return node;
+    }
+    int mid = (l + r) / 2;
+    node->left = buildTree(l, mid, initialValues);
+    node->right = buildTree(mid + 1, r, initialValues);
+
+    node->max = max(node->left->max, node->right->max);
+    return node;
+}
+
+void modify(Node* node, int l, int r, int val) {
+    if (node->r < l || node->l > r) {
+        return;
+    }
+    if (l <= node->l && node->r <= r) {
+        node->add += val;
+        node->max += val;
+        return;
+    }
+
+    modify(node->left, l, r, val);
+    modify(node->right, l, r, val);
+
+    node->max = max(node->left->max, node->right->max) + node->add;
+}
+
+int query(Node* node, int l, int r, int sumAdd) {
+    if (node->r < l || node->l > r) {
+        return INT_MIN;
+    }
+    if (l <= node->l && node->r <= r) {
+        return node->max + sumAdd;
+    }
+
+    sumAdd += node->add;
+    return max(query(node->left, l, r, sumAdd), query(node->right, l, r, sumAdd));
+}
+
+void collectValues(Node* root, int start, int end, int* output) {
+    for (int i = start; i <= end; i++) {
+        output[i] = query(root, i, i, 0);
+    }
+}
