c0e172e773786c3b0648a586dec5cb836af758a1
nastybcs <balabenkonanka@gmail.com>
2025-03-24 18:05:25 +0300

    Update lab2.cpp

--- sem2/lab2/lab2.cpp
+++ sem2/lab2/lab2.cpp
@@ -28,74 +28,6 @@ void readFile(vector<string>& sets, ifstream& inputFile) {
     inputFile.close();
 }
 
-// Собственная функция сортировки пузырьком
-void bubbleSort(vector<string>& items) {
-    int count = items.size();
-    for (int i = 0; i < count - 1; i++) {
-        for (int j = 0; j < count - i - 1; j++) {
-            if (items[j] > items[j + 1]) {
-                string temp = items[j];
-                items[j] = items[j + 1];
-                items[j + 1] = temp;
-            }
-        }
-    }
-}
-
-
-string sortSubset(const string& subset) {
-    if (subset.front() != '{' || subset.back() != '}') {
-        return subset;
-    }
-    string content = subset.substr(1, subset.size() - 2);
-    vector<string> elements;
-    string currentElement;
-    int braceDepth = 0;
-
-
-    for (char c : content) {
-        if (c == '{' && braceDepth == 0) {
-            braceDepth++;
-            currentElement += c;
-        }
-        else if (c == '}' && braceDepth > 0) {
-            braceDepth--;
-            currentElement += c;
-            if (braceDepth == 0 && !currentElement.empty()) {
-                elements.push_back(sortSubset(currentElement));
-                currentElement.clear();
-            }
-        }
-        else if (c == ',' && braceDepth == 0) {
-            if (!currentElement.empty()) {
-                elements.push_back(sortSubset(currentElement));
-                currentElement.clear();
-            }
-        }
-        else {
-            currentElement += c;
-            if (c == '{') braceDepth++;
-            else if (c == '}') braceDepth--;
-        }
-    }
-    if (!currentElement.empty()) {
-        elements.push_back(sortSubset(currentElement));
-    }
-
-
-    bubbleSort(elements);
-
-
-    string sortedSubset = "{";
-    for (int i = 0; i < elements.size(); i++) {
-        sortedSubset += elements[i];
-        if (i < elements.size() - 1) {
-            sortedSubset += ",";
-        }
-    }
-    sortedSubset += "}";
-    return sortedSubset;
-}
 
 
 vector<string> extractAndSortElements(const string& setString) {
@@ -112,13 +44,13 @@ vector<string> extractAndSortElements(const string& setString) {
             braceDepth--;
             currentElement += c;
             if (braceDepth == 0 && !currentElement.empty()) {
-                elements.push_back(sortSubset(currentElement));
+                elements.push_back(currentElement);
                 currentElement.clear();
             }
         }
         else if (c == ',' && braceDepth == 0) {
             if (!currentElement.empty()) {
-                elements.push_back(sortSubset(currentElement));
+                elements.push_back(currentElement);
                 currentElement.clear();
             }
         }
@@ -129,7 +61,7 @@ vector<string> extractAndSortElements(const string& setString) {
         }
     }
     if (!currentElement.empty()) {
-        elements.push_back(sortSubset(currentElement));
+        elements.push_back(currentElement);
     }
     return elements;
 }
