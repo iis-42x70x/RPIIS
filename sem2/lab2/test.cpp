63635c0869eb9e2ab342073d7e657ce0e6d52f9d
nastybcs <balabenkonanka@gmail.com>
2025-03-24 18:18:43 +0300

    Update test.cpp

--- sem2/lab2/test.cpp
+++ sem2/lab2/test.cpp
@@ -6,41 +6,6 @@ TEST(TestCaseName, TestName) {
   EXPECT_TRUE(true);
 }
 
-TEST(SetOperationsTest, BubbleSort) {
-    vector<string> items = { "c", "a", "b" };
-    bubbleSort(items);
-    vector<string> expected = { "a", "b", "c" };
-    EXPECT_EQ(items, expected); 
-
-    vector<string> empty;
-    bubbleSort(empty);
-    EXPECT_TRUE(empty.empty());
-}
-TEST(SetOperationsTest, SortSubset) {
-    
-    string subset = "{3,1,2}";
-    string result = sortSubset(subset);
-    EXPECT_EQ(result, "{1,2,3}");
-
-    string nested = "{3,{5,4},1}";
-    string nestedResult = sortSubset(nested);
-    EXPECT_EQ(nestedResult, "{1,3,{4,5}}");
-
-}
-TEST(SetOperationsTest, ExtractAndSortElements) {
-
-    string setString = "{3,1,2}";
-    vector<string> result = extractAndSortElements(setString);
-    vector<string> expected = { "{1,2,3}" };
-    EXPECT_EQ(result, expected);
-
-   
-    string nestedSet = "{3,{5,4},1}";
-    vector<string> nestedResult = extractAndSortElements(nestedSet);
-    vector<string> nestedExpected = { "{1,3,{4,5}}" };
-    EXPECT_EQ(nestedResult, nestedExpected);
-
-}
 TEST(SetOperationsTest, UnionSets) {
     
     vector<string> sets = { "1,2", "2,3" };
@@ -51,7 +16,21 @@ TEST(SetOperationsTest, UnionSets) {
    
     vector<string> nestedSets = { "1,{4,3}", "2,{3,4}" };
     unionSets(nestedSets, result);
-    EXPECT_EQ(result, "{1,{3,4},2}");
+    EXPECT_EQ(result, "{1,{4,3},2,{3,4}}");
 
     
+}
+TEST(SetOperationsTest, UnionSets2) {
+
+    vector<string> sets = { "a,b", "b,c" };
+    string result;
+    unionSets(sets, result);
+    EXPECT_EQ(result, "{a,b,c}");
+
+
+    vector<string> nestedSets = { "a,{b,c}", "h,{c,b}" };
+    unionSets(nestedSets, result);
+    EXPECT_EQ(result, "{a,{b,c},h,{c,b}}");
+
+
 }
