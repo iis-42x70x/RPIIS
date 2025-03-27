39fe0a078e248eb518f112d3d49e8333196adfe0
wolf1nho <artem.volkovich.2007@gmail.com>
2025-03-27 13:28:19 +0300

    Update header.h

--- sem2/lab1/header.h
+++ sem2/lab1/header.h
@@ -1,17 +1,19 @@
-#pragma once
+#ifndef PIOIVIS1LAB_HEADER_H
+#define PIOIVIS1LAB_HEADER_H
+
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
 struct Node {
+    int size;
     string ch = "";
     int freq;
     struct Node* leftchild = NULL;
     struct Node* rightchild = NULL;
     bool pushed = false;
     bool linked = false;
-    int size;
 };
 vector <pair<string, int>> fillingcounter(string str, int strlength, vector <pair<string, int>> counter);
 vector <pair<string, int>> sortcounter(int length, vector <pair<string, int>> counter);
@@ -23,4 +25,7 @@ vector <Node> linkingtree(vector <Node> tree, int treelen);
 string encode(string ch, string str, Node currentnode);
 vector <pair<string, string>> fillencodedsymbols(vector <pair<string, string>> encoding, Node currentnode);
 string showencodedstr(string encodedstr, vector <pair<string, string>> encoding, int strlength, int encodinglen, string str);
-string decode(Node currentnode, int k, string str, string encodedstr, Node tree);
+//string decode(Node currentnode, int k, string str, string encodedstr, Node tree);
+string result(string str1);
+
+#endif //PIOIVIS1LAB_HEADER_H
