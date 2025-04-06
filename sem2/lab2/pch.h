03687239dd116aa77a19be935df3f08a4ddcebe3
Артём Перфоратор <artemmarudin1@gmail.com>
2025-04-07 00:11:49 +0300

    Create pch.h

---
+++ sem2/lab2/pch.h
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <fstream>
+#include <Windows.h>
+#include <stack>
+
+using namespace std;
+
+enum ElementType { STRING, SET, ORSET };
+
+struct element {
+    ElementType type;
+    string value;
+    vector<element> setik;
+};
+
+bool letter(char);
+bool digit(char);
+bool space (char);
+bool ElementCompare(const element, const element);
+element intersection(const element, const element);
+void ElementCout(const element);
+void StringToElement(element&, string, int, int);
+bool SetUniqueCheck(element);
+bool StringCheck(string&);
+
+#endif
