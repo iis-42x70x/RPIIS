3f3e493b1768340503df9ba53c68d6bc7e53b6e9
KNM06 <nikitakuprian866@gmail.com>
2025-05-01 17:33:00 +0300

    Update pch.h

--- sem2/lab2/pch.h
+++ sem2/lab2/pch.h
@@ -2,11 +2,27 @@
 #include "gtest/gtest.h"
 #include <iostream>
 #include <vector>
-#include <string>
 #include <algorithm>
+#include <string>
+#include <fstream>
+
 using namespace std;
-bool CheckStr(const string& str);
+
+enum ElementType { STRING, SET, ORSET };
+
+struct SetElement {
+    ElementType type;
+    string strvalue;
+    vector<SetElement> vecvalue;
+};
+
 string NotSpace(string& str);
-vector<string> Fragmentation(string& str);
-void CreatePermutations(vector<string>& elements);
-bool Permutation(vector<string>& elements);
+bool CheckStr(const string& str);
+SetElement ParsingElements(const string& str, int& pos);
+SetElement ParsingSets(const string& str, int& pos);
+void Printf(const SetElement& el);
+vector<vector<SetElement>> Permute(const vector<SetElement>& elems);
+void GenerateCombinations(const vector<vector<SetElement>>& childOpts, vector<vector<SetElement>>& combos, vector<SetElement>& cur, int i);
+void BuildCombinations(const vector<vector<SetElement>>& options, vector<vector<SetElement>>& base, vector<SetElement>& cur, int i);
+bool CompareElements(const SetElement& a, const SetElement& b);
+string ReadFromFile();
