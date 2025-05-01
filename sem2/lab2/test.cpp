da3068356fb24b1b43d2cc7f2398ba4e2239ea86
KNM06 <nikitakuprian866@gmail.com>
2025-05-01 17:31:45 +0300

    Update test.cpp

--- sem2/lab2/test.cpp
+++ sem2/lab2/test.cpp
@@ -1,10 +1,5 @@
 #include "pch.h"
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
 using namespace std;
-
 TEST(TestNotSpace, TestNotSpace1) {
 	string str1 = "B = { o , < 1, 2 > , A2 ,c 3 ,B ,b3 _ A, { } , {o , { } , A} }";
 	string str2 = "B={o,<1,2>,A2,c3,B,b3_A,{},{o,{},A}}";
@@ -33,27 +28,4 @@ TEST(TestCheckStr, TestCheckStr5) {
 TEST(TestCheckStr, TestCheckStr6) {
 	string str = "";
 	EXPECT_EQ(CheckStr(str), false);
-}
-TEST(TestPermutation, TestPermutation1) {
-	vector<string> a = { "f","e","d","c","b","a" };
-	EXPECT_EQ(Permutation(a), false);
-}
-TEST(TestPermutation, TestPermutation2) {
-	vector<string> a = { "f","e","d","c","b","a","b"};
-	EXPECT_EQ(Permutation(a), true);
-}
-TEST(TestFragmentation, TestFragmentation1) {
-	string str = "{f,{e,d},c,<b,a>,b}";
-	vector<string> a = { "f","{e,d}","c","<b,a>","b" };
-	EXPECT_EQ(Fragmentation(str), a);
-}
-TEST(TestFragmentation, TestFragmentation2) {
-	string str = "f,{e,d},c,<b,a>,b";
-	vector<string> a = { "f","{e,d}","c","<b,a>","b" };
-	EXPECT_EQ(Fragmentation(str), a);
-}
-int main(int argc, char** argv) {
-	setlocale(LC_ALL, "RU");
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
 }
