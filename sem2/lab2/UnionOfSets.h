f5150537edf4698c1d6a4d2f22ad3373593046cd
Tatyana305-png <tanchik07k@gmail.com>
2025-05-04 17:35:32 +0300

    Update UnionOfSets.h

--- sem2/lab2/UnionOfSets.h
+++ sem2/lab2/UnionOfSets.h
@@ -1,1 +1,13 @@
+#pragma once
 
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void run_testcase(const string& path, int& num_sets, vector<vector<string>>& sets);
+vector<vector<pair<string, int>>> CountMultiplicities(const vector<vector<string>>& sets);
+vector<pair<string, int>> FindMaxMultiplicities(const vector<vector<pair<string, int>>>& counts);
+void OutputResult(const vector<pair<string, int>>& max_counts);
